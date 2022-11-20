4d102d0d6eaa2b0c00e2f0de453bb5bcc55f264f
Sonia <tuculia_sonia@yahoo.com>
2022-11-20 18:22:03 +0200

    Added implementation for the second homework

---
+++ build/CMakeFiles/paoo_tema2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/paoo_tema2.dir/main.cpp.o"
+  "CMakeFiles/paoo_tema2.dir/main.cpp.o.d"
+  "paoo_tema2"
+  "paoo_tema2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/paoo_tema2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,33 +1,78 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+class Design{
+    private:
+        float weight;
+        string color;
+
+    public:
+        Design()
+        :weight(1),
+         color("black")
+         {}
+
+        Design(const float w, const string c)
+        :weight(w),
+         color(c)
+         {}
+        
+        Design(const Design &design)
+        :weight(design.weight),
+         color(design.color)
+         {}
+
+        float getWeight(){
+            return weight;
+        }
+
+        string getColor(){
+            return color;
+        }
+};
 
 class ComplexNumber{
     protected:
         float re;
         float img;
+        Design* design;
 
     public:
         ComplexNumber()
         :re(0),
-         img(0)
+         img(0),
+         design(new Design())
          {}
 
-        ComplexNumber(const float a, const float b)
+        ComplexNumber(const float a, const float b, Design d)
         :re(a),
-         img(b)
+         img(b),
+         design(&d)
          {}
 
         ComplexNumber(const ComplexNumber& number){
             cout<<"In copy constructor"<<endl;
             re=number.img;
             img=number.re;
+            design=new Design(*number.design);
         }
 
         ComplexNumber& operator=(const ComplexNumber& number){
             cout<<"In copy assignment operator"<<endl;
             re=number.re;
             img=-number.img;
+            Design *dOrig=design;
+            design=new Design(*number.design);
+            delete dOrig;
+            return *this;
+        }
+
+        ComplexNumber& operator+=(const ComplexNumber& number){
+            cout<<"In += operator"<<endl;
+            re+=number.re;
+            img+=number.img;
             return *this;
         }
 
@@ -39,48 +84,69 @@ class ComplexNumber{
             return img;
         }
 
-        void setRe(float a){
-            re=a;
-        }
-
-        void setImg(float b){
-            img=b;
+        Design getDesign(){
+            return *design;
         }
 };
 
-class Uncopyable {
-    protected: 
-        Uncopyable() {} 
-        ~Uncopyable() {} 
-        Uncopyable(const Uncopyable&)=delete; 
-        Uncopyable& operator=(const Uncopyable&)=delete;
-};
-
-class UncopyableComplexNumber : public ComplexNumber, public Uncopyable{
+class StilishedComplexNumber : public ComplexNumber{
+    private:
+        string style;
     public:
-        UncopyableComplexNumber(const float a, const float b)
-            :ComplexNumber(a,b)
-            {}
-    //using ComplexNumber::ComplexNumber;
+        StilishedComplexNumber()
+        :ComplexNumber(),
+         style("solid")
+         {}
+
+        StilishedComplexNumber(const float a, const float b, Design d, const string s)
+        :ComplexNumber(a,b,d),
+         style(s)
+         {}
+
+        StilishedComplexNumber(const StilishedComplexNumber& number)
+        :ComplexNumber(number),
+         style(number.style)
+         {
+            cout<<"In stilished copy constructor"<<endl;
+         }
+ 
+        StilishedComplexNumber& operator=(const StilishedComplexNumber& number){
+            cout<<"In stilished copy assignment operator"<<endl;
+            ComplexNumber::operator=(number);
+            style=number.style;
+            return *this;
+        }
+
+        string getStyle(){
+            return style;
+        }
 };
 
 int main()
 {
-    int *a;
-    char b;
-    cout<<"a = "<<a<<endl;
-    cout<<"b = "<<b<<endl;
+    Design design(1.5,"red");
+    // ComplexNumber number1(2,3,design);
+    // cout<<"number1 = "<<number1.getRe()<<" + "<<number1.getImg()<<"i"<<"; weight:"<<number1.getDesign().getWeight()<<" pt; color:"<<number1.getDesign().getColor()<<endl;
+    // ComplexNumber number2(number1);
+    // cout<<"number2 = "<<number2.getRe()<<" + "<<number2.getImg()<<"i"<<"; weight:"<<number2.getDesign().getWeight()<<" pt; color:"<<number2.getDesign().getColor()<<endl;
+    // ComplexNumber number3, number4;
+    // cout<<"number3 = "<<number3.getRe()<<" + "<<number3.getImg()<<"i"<<"; weight:"<<number3.getDesign().getWeight()<<" pt; color:"<<number3.getDesign().getColor()<<endl;
+    // number4 = number3 = number1;
+    // cout<<"number3 = "<<number3.getRe()<<" + "<<number3.getImg()<<"i"<<"; weight:"<<number3.getDesign().getWeight()<<" pt; color:"<<number3.getDesign().getColor()<<endl;
+    // cout<<"number4 = "<<number4.getRe()<<" + "<<number4.getImg()<<"i"<<"; weight:"<<number4.getDesign().getWeight()<<" pt; color:"<<number4.getDesign().getColor()<<endl;
+    // number4+=number3+=number2;
+    // cout<<"number4 = "<<number4.getRe()<<" + "<<number4.getImg()<<"i"<<"; weight:"<<number4.getDesign().getWeight()<<" pt; color:"<<number4.getDesign().getColor()<<endl;
+    // number2 = number2;
 
-    ComplexNumber number(2,3);
-    cout<<"number = "<<number.getRe()<<" + "<<number.getImg()<<"i"<<endl;
-    ComplexNumber number2(number);
-    cout<<"number2 = "<<number2.getRe()<<" + "<<number2.getImg()<<"i"<<endl;
-    ComplexNumber number3;
-    number3 = number;
-    cout<<"number3 = "<<number3.getRe()<<" + "<<number3.getImg()<<"i"<<endl;
-
-    UncopyableComplexNumber uncopyableNumber(5,7);
-    cout<<"uncopyable = "<<uncopyableNumber.getRe()<<" + "<<uncopyableNumber.getImg()<<"i"<<endl;
-    //UncopyableComplexNumber copy(uncopyableNumber);
+    try{
+        StilishedComplexNumber stilishedNumber(5,7,design,"dotted");
+        cout<<"stilishedNumber = "<<stilishedNumber.getRe()<<" + "<<stilishedNumber.getImg()<<"i"<<"; style:"<<stilishedNumber.getStyle()<<endl;
+        cout<<stilishedNumber.getDesign().getColor();
+        StilishedComplexNumber stilishedNumber2;
+        stilishedNumber2 = stilishedNumber;
+    }
+    catch (const bad_alloc& e) {
+        cout << "Allocation failed: " << e.what() <<endl;
+    }
     return 0;
 }
